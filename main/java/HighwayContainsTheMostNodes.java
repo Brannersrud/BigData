16368cc5405c00d08dac777b35a11e49155d8c0a
Anders Saxrud <brannersrud@gmail.com>
2019-10-03 11:14:51 +0200

    added new file

--- main/java/HighwayContainsTheMostNodes.java
+++ main/java/HighwayContainsTheMostNodes.java
@@ -172,126 +172,6 @@ public class HighwayContainsTheMostNodes {
 
 
 
-	public static class StartEndFileInputFormat extends FileInputFormat < LongWritable, Text > {
-
-		@Override
-		public RecordReader < LongWritable,
-				Text > createRecordReader(
-				InputSplit split, TaskAttemptContext context) throws IOException,
-				InterruptedException {
-
-			StartEndRecordReader reader = new StartEndRecordReader();
-			reader.initialize(split, context);
-
-			return reader;
-		}
-
-	}
-
-	public static class StartEndRecordReader extends RecordReader < LongWritable, Text > {
-
-		private long start;
-		private long pos;
-		private long end;
-		private FSDataInputStream fsin;
-		private byte[] startTag;
-		private byte[] endTag;
-		private LongWritable key = new LongWritable();
-		private Text value = new Text();
-		private final DataOutputBuffer buffer = new DataOutputBuffer();
-
-
-		@Override
-		public void initialize(InputSplit genericSplit, TaskAttemptContext context) throws IOException {
-			FileSplit split = (FileSplit) genericSplit;
-
-
-			Configuration job = context.getConfiguration();
-			this.startTag = job.get("com.geitle.startend.startTag").getBytes("utf-8");
-			this.endTag = job.get("com.geitle.startend.endTag").getBytes("utf-8");
-
-
-			start = split.getStart();
-			end = start + split.getLength();
-
-			final Path file = split.getPath();
-			FileSystem fs = file.getFileSystem(job);
-			this.fsin = fs.open(split.getPath());
-			fsin.seek(start);
-		}
-
-
-		@Override
-		public boolean nextKeyValue() throws IOException {
-			if (fsin.getPos() < end) {
-				if (readUntilMatch(startTag, false)) {
-					try {
-						buffer.write(startTag);
-						if (readUntilMatch(endTag, true)) {
-							key.set(fsin.getPos());
-							value.set(buffer.getData(), 0, buffer.getLength());
-							return true;
-						}
-					} finally {
-						buffer.reset();
-					}
-				}
-			}
-			return false;
-		}
-
-		private boolean readUntilMatch(byte[] match, boolean withinBlock) throws IOException {
-			int i = 0;
-			while (true) {
-				int b = fsin.read();
-				// end of file:
-				if (b == -1) return false;
-				// save to buffer:
-				if (withinBlock) buffer.write(b);
-
-				// check if we're matching:
-				if (b == match[i]) {
-					i++;
-					if (i >= match.length) return true;
-				} else i = 0;
-				// see if we've passed the stop point:
-				if (!withinBlock && i == 0 && fsin.getPos() >= end) return false;
-			}
-		}
-
-
-
-		@Override
-		public LongWritable getCurrentKey() throws IOException,
-				InterruptedException {
-			return key;
-		}
-
-		@Override
-		public Text getCurrentValue() throws IOException,
-				InterruptedException {
-			return value;
-		}
-
-
-		@Override
-		public float getProgress() throws IOException,
-				InterruptedException {
-			if (start == end) {
-				return 0.0f;
-			} else {
-				return Math.min(1.0f, (pos - start) / (float)(end - start));
-			}
-		}
-
-
-		@Override
-		public void close() throws IOException {
-			if (fsin != null) {
-				fsin.close();
-			}
-		}
-	}
 
 	public static class TokenizerMapper
 			extends Mapper < Object, Text, Text, IntWritable > {
@@ -395,8 +275,7 @@ public class HighwayContainsTheMostNodes {
 
 */
 
-
-		job.setInputFormatClass(StartEndFileInputFormat.class);
+		job.setInputFormatClass(XMLmapper.StartEndFileInputFormat.class);
 		job.setMapOutputValueClass(IntWritable.class);
 
 		//textintpar
