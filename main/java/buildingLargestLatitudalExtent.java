bbb211d5235ebebd99c9fad50ea963d199f977d0
Anders Saxrud <brannersrud@gmail.com>
2019-10-04 21:17:12 +0200

    added alot of frekkinn logic

--- main/java/buildingLargestLatitudalExtent.java
+++ main/java/buildingLargestLatitudalExtent.java
@@ -1,6 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -20,16 +20,13 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.stream.Collectors.toMap;
 
 public class buildingLargestLatitudalExtent {
 
-	public static class TokenizerMapper extends Mapper< Object, Text, Text, IntWritable > {
+	public static class TokenizerMapper extends Mapper< Object, Text, Text, DoubleWritable > {
 		private Text word = new Text();
 		int max = Integer.MIN_VALUE;
 
@@ -45,36 +42,39 @@ public class buildingLargestLatitudalExtent {
 				Element root = document.getDocumentElement();
 				NodeList nodeList = root.getElementsByTagName("node");
 
-				String name ="";
-				int lat=0;
+				double lat=0;
 				Boolean isBuilding = false;
-				HashMap<String, Integer> myhashmap = new HashMap<>();
+				HashMap<String, Double> myhashmap = new HashMap<>();
+				String name="";
 				for (int i = 0, len = nodeList.getLength(); i < len; i++) {
 					Node node = nodeList.item(i);
 					if(node.hasChildNodes()){
 						for(int j = 0, jlen = node.getChildNodes().getLength(); j < jlen; j++){
+							if(node.getChildNodes().item(j).getNodeName().equals("tag")){
 									if(node.getChildNodes().item(j).getAttributes().getNamedItem("k").getTextContent().equals("building")){
 										isBuilding = true;
-										lat = Integer.parseInt(node.getAttributes().getNamedItem("lat").getTextContent());
+										lat = Double.parseDouble(node.getAttributes().getNamedItem("lat").getTextContent());
 									}else if(node.getChildNodes().item(j).getAttributes().getNamedItem("k").getTextContent().equals("name")){
 										name = node.getChildNodes().item(j).getAttributes().getNamedItem("v").getTextContent();
+
 									}
 
 
+						}
 
 						}
 						if(isBuilding){
 							myhashmap.put(name, lat);
 						}
-						isBuilding = true;
-
+						isBuilding = false;
 					}
 
+
 				}
-				Comparator<Map.Entry<String, Integer>> valueComparator =
+				Comparator<Map.Entry<String, Double>> valueComparator =
 						(e1, e2) -> e1.getKey().compareTo(e2.getKey());
 
-				Map<String, Integer> sorted = myhashmap
+				Map<String, Double> sorted = myhashmap
 						.entrySet()
 						.stream()
 						.sorted(valueComparator.reversed())
@@ -82,15 +82,44 @@ public class buildingLargestLatitudalExtent {
 								Map.Entry::getKey, Map.Entry::getValue, (e1,e2) -> e1, LinkedHashMap::new));
 
 				int mapcounter=0;
-				for (Map.Entry<String, Integer> vals : sorted.entrySet()) {
-					if (!(mapcounter > 20)){
-						mapcounter++;
-						context.write(new Text(vals.getKey()), new IntWritable(vals.getValue()));
-
+				String nameOfVal = "placeholder";
+				double longlat = 0;
+				double shortlat=Double.MAX_VALUE;
+				Map<String, Double> secondHashmap = new HashMap<String, Double>();
+				for (Map.Entry<String, Double> vals : sorted.entrySet()) {
+					if(nameOfVal.equals("placeholder")){
+						nameOfVal = vals.getKey();
 					}
-
-
+					if(vals.getKey() != nameOfVal){
+						Double valueToWrite = longlat-shortlat;
+						secondHashmap.put(nameOfVal, valueToWrite);
+						nameOfVal = vals.getKey();
+					}
+					if(nameOfVal == vals.getKey()){
+						if(vals.getValue() > longlat){
+							longlat = vals.getValue();
+						}
+						else if(vals.getValue() < shortlat){
+							shortlat = vals.getValue();
+						}
+					}
 				}
+
+
+				Iterator<Map.Entry<String, Double>> it = secondHashmap.entrySet().iterator();
+				Double highest = 0.0;
+				String nameOfBuilding = "";
+
+				while(it.hasNext()){
+					Map.Entry<String, Double> pair = (Map.Entry<String, Double>) it.next();
+
+					if(pair.getValue() > highest){
+						highest = pair.getValue();
+						nameOfBuilding = pair.getKey();
+					}
+				}
+				context.write(new Text(nameOfBuilding), new DoubleWritable(highest));
+
 
 
 			} catch (SAXException exception) {
@@ -103,17 +132,17 @@ public class buildingLargestLatitudalExtent {
 	}
 
 	public static class IntSumReducer
-			extends Reducer< Text, IntWritable, Text, IntWritable > {
-		private IntWritable result = new IntWritable();
-
-		public void reduce(Text key, Iterable < IntWritable > values, Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			Text n = new Text();
-			while(values.iterator().hasNext()){
-				sum = values.iterator().next().get();
-				n = key;
+			extends Reducer< Text, DoubleWritable, Text, DoubleWritable > {
+		private DoubleWritable result = new DoubleWritable();
+		public void reduce(Text key, Iterable < DoubleWritable > values, Context context) throws IOException, InterruptedException {
+			Double sum = 0.0;
+			String keyvalue = "";
+			for (DoubleWritable val: values) {
+					sum = val.get();
+					keyvalue = key.toString();
 			}
-			context.write(n, new IntWritable(sum));
+			result.set(sum);
+			context.write(new Text("the highest latitude extent: " + keyvalue), result);
 
 		}
 	}
@@ -125,13 +154,13 @@ public class buildingLargestLatitudalExtent {
 		conf.set("com.geitle.startend.startTag", "<osm");
 		conf.set("com.geitle.startend.endTag", "</osm>");
 		Job job = Job.getInstance(conf, "xml count");
-		job.setJarByClass(FindMostupdatedObject.class);
-		job.setMapperClass(FindMostupdatedObject.TokenizerMapper.class);
-		job.setReducerClass(FindMostupdatedObject.IntSumReducer.class);
+		job.setJarByClass(buildingLargestLatitudalExtent.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setReducerClass(IntSumReducer.class);
 		job.setSortComparatorClass(LongWritable.DecreasingComparator.class);
 		job.setInputFormatClass(XMLmapper.StartEndFileInputFormat.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
